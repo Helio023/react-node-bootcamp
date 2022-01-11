c6538efc6c765d1c32252517739b60cb29b1eb5a
unknown <helioengracialipinze@gmail.com>
2022-01-11 01:50:37 -0800

    Forntend login added

--- frontend/src/core/pages/Admin/components/Products/List/List .tsx
+++ frontend/src/core/pages/Admin/components/Products/List/List .tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import './styles.scss'
 
 const List = () => {
   const navigate = useNavigate();
--- frontend/src/core/pages/Auth/componets/Login/Login.tsx
+++ frontend/src/core/pages/Auth/componets/Login/Login.tsx
@@ -1,35 +1,53 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import AuthButton from '../Button/AuthButton';
 import AuthCard from '../Card/AuthCard';
 import { useForm } from 'react-hook-form';
 import './styles.scss';
+import axios from 'axios';
 
 type FormData = {
   email: string;
   password: string;
 };
 
+const BASE_URL = 'http://localhost:3000/api/v1/catalog';
+
 const Login = () => {
   const { register, handleSubmit } = useForm<FormData>();
+  const [hasError, setHasError] = useState(false);
+  const navigate = useNavigate()
 
   const onSubmit = (data: FormData) => {
-    console.log(data);
+    axios
+      .post(`${BASE_URL}/login`, data)
+      .then((res) => {
+        setHasError(false);
+        navigate('/admin/products')
+      })
+      .catch(() => {
+        setHasError(true);
+      });
   };
+
   return (
     <div className='login'>
       <AuthCard title='Entrar'>
+        {hasError && (
+          <div className='login-alert'>Email ou senha inválidos</div>
+        )}
         <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
           <input
             type='email'
             className='input-base form-control mb-4'
             placeholder='Email'
-            {...register('email')}
+            {...register('email', { required: true })}
           />
           <input
             type='password'
             className='input-base form-control'
             placeholder='Senha'
-            {...register('password')}
+            {...register('password', { required: true, minLength: 8 })}
           />
           <Link to='/admin/auth/recover' className='login-recover-link'>
             Esqueceu a senha?
