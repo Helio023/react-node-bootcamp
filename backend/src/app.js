35d7b6758d1e23b8111915475e592db6499922e0
unknown <helioengracialipinze@gmail.com>
2022-01-11 01:49:35 -0800

    Forntend login added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ path: '.env' });
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const errorHandler = require('./controllers/errorController');
 const connectDb = require('./database/connectMongoDb');
 const app = express();
@@ -9,6 +10,7 @@ connectDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
 
 app.use('/api/v1/catalog', require('./routes/productRoutes'));
 app.use('/api/v1/catalog', require('./routes/authRoutes'));
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,8 +2,19 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const ErrorResponse = require('../utils/errorResponse');
 
-const createToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = user.signToken();
+
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
+
+  // Remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
@@ -21,7 +32,8 @@ exports.register = async (req, res, next) => {
       passwordConfirm,
     });
 
-    createToken(user, 201, res);
+    // createToken(user, 201, res);
+    createSendToken(user, 201, req, res);
   } catch (error) {
     next(error);
   }
@@ -41,7 +53,8 @@ exports.login = async (req, res, next) => {
       return next(new ErrorResponse('Invalid credentials', 401));
     }
 
-    createToken(user, 200, res);
+    // createToken(user, 200, res);
+    createSendToken(user, 200, req, res);
   } catch (error) {
     next(error);
   }
@@ -54,6 +67,8 @@ exports.protect = async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token) {
